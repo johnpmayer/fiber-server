ee65b8f92eeeff02d6d900c3f54dd9f6642b1c1b
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2014-04-06 23:55:40 -0400

    bit better. stuck on 'blocking' read

--- src/main/java/com/wecamtoplay/Server.java
+++ src/main/java/com/wecamtoplay/Server.java
@@ -4,7 +4,9 @@ package com.wecamtoplay;
 import co.paralleluniverse.fibers.*;
 import co.paralleluniverse.fibers.io.*;
 import co.paralleluniverse.strands.*;
-import java.io.*;
+import java.io.IOException;
+import java.nio.*;
+import java.nio.channels.*;
 import java.net.*;
 import java.util.concurrent.*;
 
@@ -22,38 +24,41 @@ public class Server {
 
     @Override
     public void run() throws SuspendExecution {
+      System.out.println("Handling client");
+      ByteBuffer inBuffer = ByteBuffer.allocate(1);
       try {
-        System.out.println("Handling client... just closing it");
+        while (true) {
+          int bytesRead = this.chan.read(inBuffer);
+          System.out.println("read stuff: " + bytesRead);
+          if (bytesRead < 1) { break; }
+          int bytesWritten = this.chan.write(inBuffer);
+          System.out.println("wrote stuff: " + bytesWritten);
+        }
         this.chan.close();
+        System.out.println("Closed.");
       } catch (IOException e) {
+        System.out.println(e.toString());
         throw new RuntimeException(e);
       }
     }
 
   }
 
-  public class AcceptLoop implements SuspendableRunnable {
-    @Override
-    public void run() throws SuspendExecution {
+  public void start() {
+    Fiber<Void> acceptLoop = new  Fiber<Void>((SuspendableRunnable)() -> {
       try {
-        System.out.println("Binding to address");
         FiberServerSocketChannel socket = FiberServerSocketChannel.open().bind(new InetSocketAddress(PORT));
         System.out.println("Listening...");
         while (true) {
           FiberSocketChannel chan = socket.accept();
-          System.out.println("Accepted a client");
-          Fiber clientHandler = new Fiber(new ClientHandler(chan));
+          Fiber<Void> clientHandler = new Fiber<Void>(new ClientHandler(chan));
           System.out.println("Forking a client handler fiber");
           clientHandler.start(); // No join
         }
       } catch (IOException e) {
         throw new RuntimeException(e);
       }
-    }
-  }
-
-  public void start() {
-    Fiber acceptLoop = new Fiber(new AcceptLoop());
+    });
     acceptLoop.start();
     try {
       acceptLoop.join();
